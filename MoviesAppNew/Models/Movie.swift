6f651555b3fa4c495d2ed11c7962e4dfd70eba70
AaronGarman <aaron.a.garman14@gmail.com>
2025-11-06 15:21:14 -0500

    Added movie model

--- MoviesAppNew/Models/Movie.swift
+++ MoviesAppNew/Models/Movie.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct MoviesApiResponse: Decodable {
+    let results: [Movie]
+}
+
+struct Movie: Decodable {
+    let title: String
+    let overview: String
+    let posterPath: String?
+    let backdropPath: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case title
+        case overview
+        case posterPath = "poster_path"
+        case backdropPath = "backdrop_path"
+    }
+}
+
+// diff file name/structure any?
+// more attributes pull?
