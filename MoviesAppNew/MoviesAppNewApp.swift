43cf382c22061db268fe2ddb4e6d6468ce70e119
AaronGarman <aaron.a.garman14@gmail.com>
2025-11-06 15:03:45 -0500

    Added initial source code files

---
+++ MoviesAppNew/MoviesAppNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppNewApp.swift
+//  MoviesAppNew
+//
+//  Created by Aaron Garman on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
